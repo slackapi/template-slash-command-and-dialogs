65a54ca8d9b672fa9b2e657b8f3065fc1dc0e2b7
David Pichsenmeister <dpichsenmeister@slack-corp.com>
2019-12-19 13:23:44 -0800

    update to granular permissions

--- src/verifySignature.js
+++ src/verifySignature.js
@@ -5,7 +5,6 @@ const isVerified = (req) => {
   const signature = req.headers['x-slack-signature'];
   const timestamp = req.headers['x-slack-request-timestamp'];
   const hmac = crypto.createHmac('sha256', process.env.SLACK_SIGNING_SECRET);
-  console.log('signature', signature)
   const [version, hash] = signature.split('=');
 
   // Check if the timestamp is too old
